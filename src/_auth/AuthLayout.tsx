8ba7ffeff014dde792b6fa2e31ca0d8bda1da455
Shankar <nandashankar26@gmail.com>
2023-11-18 15:41:51 +0530

    first commit

---
+++ src/_auth/AuthLayout.tsx
@@ -0,0 +1,26 @@
+import { useUserContext } from "@/context/AuthContext";
+import { Outlet, Navigate } from "react-router-dom";
+
+export default function AuthLayout() {
+  const { isAuthenticated } = useUserContext();
+
+  return (
+    <>
+      {isAuthenticated ? (
+        <Navigate to="/" />
+      ) : (
+        <>
+          <section className="flex flex-1 justify-center items-center flex-col py-10">
+            <Outlet />
+          </section>
+
+          <img
+            src="/assets/images/side-img.svg"
+            alt="logo"
+            className="hidden xl:block h-screen w-1/2 object-cover bg-no-repeat"
+          />
+        </>
+      )}
+    </>
+  );
+}
---
+++ src/_root/pages/Home.tsx
@@ -0,0 +1,53 @@
+import Loader from "@/components/shared/Loader";
+import PostCard from "@/components/shared/PostCard";
+import { useGetRecentPosts, useGetUsers } from "@/lib/react-query/queriesAndMutations";
+import { Models } from "appwrite";
+
+const Home = () => {
+  const {
+    data: posts,
+    isPending: isPostLoading,
+    isError: isErrorPosts,
+  } = useGetRecentPosts();
+
+  const {
+    isError: isErrorCreators,
+  } = useGetUsers(10);
+
+  if (isErrorPosts || isErrorCreators) {
+    return (
+      <div className="flex flex-1">
+        <div className="home-container">
+          <p className="body-medium text-light-1">Something bad happened</p>
+        </div>
+        <div className="home-creators">
+          <p className="body-medium text-light-1">Something bad happened</p>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex flex-1">
+      <div className="home-container">
+        <div className="home-posts">
+          <h2 className="h3-bold md:h2-bold text-left w-full">Home Feed</h2>
+          {isPostLoading && !posts ? (
+            <Loader />
+          ) : (
+            <ul className="flex flex-col flex-1 gap-9 w-full ">
+              {posts?.documents.map((post: Models.Document) => (
+                <li key={post.$id} className="flex justify-center w-full">
+                  <PostCard post={post} />
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
+
